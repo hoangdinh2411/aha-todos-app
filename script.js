8948a5badc37ec842c4045d784b314e3987a2cbc
hoangdinh2411 <hoangdinh2411@gmail.com>
2022-04-04 20:46:42 +0200

    update server worker

--- script.js
+++ script.js
@@ -2,8 +2,13 @@ window.addEventListener('load',async()=>{
     if('serverWorker' in navigator){
         try {
             await navigator.serviceWorker.register('serviceworker.js')
+            .then(function(registration) {
+                console.log('Service worker registration succeeded:', registration);
+              }, /*catch*/ function(error) {
+                console.log('Service worker registration failed:', error);
+              });
         } catch (error) {
             console.log(error)
         }
     }
-})
+});
