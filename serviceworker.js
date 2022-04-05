a8db9359c4dc9148c3f7ff395583b90272b65a33
hoangdinh2411 <hoangdinh2411@gmail.com>
2022-04-05 10:51:31 +0200

    update service work install and activate

---
+++ serviceworker.js
@@ -0,0 +1,53 @@
+
+const cacheName = 'v1';
+const cacheAssets = [
+  'index.html',
+  'js/app.js',
+  'scss/main.css',
+];
+
+self.addEventListener('install', (e) => {
+  e.waitUtil(
+    caches
+      .open(cacheName)
+      .then((cache) => cache.addAll(cacheAssets))
+      .then(() => self.skipWaiting())
+  );
+});
+
+
+self.addEventListener('activate',(e)=>{
+    e.waitUtil(caches.keys().then(cacheNames=>{
+        return Promise.all(
+            cacheNames.map(cache=>{
+                if(cache !== cacheName){
+                    return caches.delete(cache)
+                }
+            })
+        )
+    }))
+});
+
+self.addEventListener("fetch", (event) => {
+  console.log(event)
+  console.log('fetching server worker')
+    event.respondWith(
+        fetch(event.request)
+            .then((res) => {
+                //Make clone of response
+                const resClone = res.clone();
+                // Open cache
+                caches.open(cacheName).then((cache) => {
+                    // Add response to the cache
+                    cache.put(event.request, resClone);
+                });
+                return res;
+            })
+            .catch((err) =>
+                caches
+                    .match(event.request)
+                    .then((res) => res)
+                    .catch((err) => console.error(err))
+            )
+    );
+});
